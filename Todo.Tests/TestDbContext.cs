48c73c22f876e13c7968d4fdc6ecef4244713026
RobertGilmour <robert@gilmouruk.com>
2020-05-14 15:25:07 +0100

    Task 7 - Change relevant todo lists to include those the user has a todo item on
    Fix unit test

---
+++ Todo.Tests/TestDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Todo.Data;
+
+namespace Todo.Tests
+{
+    public class TestDbContext : ApplicationDbContext
+    {
+        public TestDbContext() : base(
+            new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlite(CreateInMemoryDatabase()).Options)
+        {
+        }
+
+        private static DbConnection CreateInMemoryDatabase()
+        {
+            var connection = new SqliteConnection("Filename=:memory:");
+
+            connection.Open();
+
+            return connection;
+        }
+
+        public override void Dispose()
+        {
+            Database.GetDbConnection().Dispose();
+            base.Dispose();
+        }
+    }
+}
---
+++ Todo.Tests/WhenFetchingRelevantTodoLists.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using Todo.Data.Entities;
+using Todo.Services;
+using Xunit;
+
+namespace Todo.Tests
+{
+    public class WhenFetchingRelevantTodoLists
+    {
+        private readonly List<TodoList> resultTodoLists;
+        private readonly TodoList listWhereUserIsOwner;
+        private readonly TodoList listWhereUserIsResponsibleForItem;
+        private readonly TodoList listWhereUserIsBothOwnerAndResponsibleForItem;
+        private readonly TodoList irrelevantList;
+
+        public WhenFetchingRelevantTodoLists()
+        {
+            var user = new IdentityUser("alice@example.com");
+            var otherUser = new IdentityUser("bob@example.com");
+
+            listWhereUserIsOwner = new TodoList(user, "User is owner");
+            listWhereUserIsResponsibleForItem = new TodoList(otherUser, "User is responsible for item");
+            listWhereUserIsBothOwnerAndResponsibleForItem = new TodoList(user, "User is owner and responsible for item");
+            irrelevantList = new TodoList(otherUser, "Irrelevant list");
+
+            using (var ctx = new TestDbContext())
+            {
+                ctx.Database.EnsureDeleted();
+                ctx.Database.EnsureCreated();
+
+                ctx.AddRange(listWhereUserIsOwner, 
+                    listWhereUserIsResponsibleForItem,
+                    listWhereUserIsBothOwnerAndResponsibleForItem, 
+                    irrelevantList);
+
+                listWhereUserIsOwner.Items
+                    .Add(new TodoItem(listWhereUserIsOwner.TodoListId, otherUser.Id, "Todo 1", Importance.Medium));
+
+                listWhereUserIsResponsibleForItem.Items
+                    .Add(new TodoItem(listWhereUserIsResponsibleForItem.TodoListId, user.Id, "Todo 2", Importance.Medium));
+
+                listWhereUserIsBothOwnerAndResponsibleForItem.Items
+                    .Add(new TodoItem(listWhereUserIsBothOwnerAndResponsibleForItem.TodoListId, user.Id, "Todo 3", Importance.Medium));
+
+                irrelevantList.Items
+                    .Add(new TodoItem(listWhereUserIsOwner.TodoListId, otherUser.Id, "Todo 4", Importance.Medium));
+
+                ctx.SaveChanges();
+
+                resultTodoLists = ctx.RelevantTodoLists(user.Id).ToList();
+            }
+        }
+
+        [Fact]
+        public void ContainsListWhereUserIsOwner()
+        {
+            Assert.Contains(resultTodoLists, item => item.TodoListId == listWhereUserIsOwner.TodoListId);
+        }
+
+        [Fact]
+        public void ContainsListWhereUserIsResponsibleForTodoItem()
+        {
+            Assert.Contains(resultTodoLists, item => item.TodoListId == listWhereUserIsResponsibleForItem.TodoListId);
+        }
+
+        [Fact]
+        public void ContainsListWhereUserIsOwnerAndResponsibleForTodoItem()
+        {
+            Assert.Contains(resultTodoLists, item => item.TodoListId == listWhereUserIsBothOwnerAndResponsibleForItem.TodoListId);
+        }
+
+        [Fact]
+        public void DoesNotContainIrrelevantList()
+        {
+            Assert.DoesNotContain(resultTodoLists, item => item.TodoListId == irrelevantList.TodoListId);
+        }
+    }
+}
